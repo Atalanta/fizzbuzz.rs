d05aa7ef1a77bf9b3d0e8433725f93fe7d84bd44
SNS <stephen.nelsonsmith@ticketmaster.co.uk>
2025-03-02 14:04:16 +0000

    Change: refactor to use Enum as planned

--- README.md
+++ README.md
@@ -9,16 +9,21 @@ This is an example implementation of the classic FizzBuzz kata in Rust.
 - If the number is divisible by 5, instead of printing the number, the word "buzz" should be printed.
 - If the number is divisible by 3 and 5, instead of printing the number, the word "fizzbuzz" should be printed.
 
+## Tests
+
+Run the tests with:
+
+```shell-session
+cargo test
+```
+
+## Run
+
+Run the program with:
+
+```shell-session
+cargo run
+```
 ## Design considerations
 
-This solution is influenced by my background in Ruby and Erlang.
-
-Rust has expressive pattern matching capabilities, akin to Erlang's `case` expression, and functional-style iterator chaining akin to Ruby's blocks.  I wanted to showcase this functionality.  The tuple matching is particularly elegant.
-
-I elected to return a `String` for each case, since we need to print the result.
-
-## Improvements
-
-Calling `to_string()` on each match arm is a bit inelegant.  An improvement would be to define a `FizzBuzzResult` Enum with variants for each option.  This would also make adding further rules nicely readable
-
-We could then implement the `Display` trait for `FizzBuzzResult`, which would handle string conversion, and would also allow for different output options.
+Rust's expressive pattern matching and type system encourage a type-driven approach.
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
-fn fizzbuzz(number: u8) -> String {
+use std::fmt::{Display, Formatter, Result};
+
+#[derive(Debug, PartialEq)]
+enum FizzBuzz {
+    Fizz,
+    Buzz,
+    FizzBuzz,
+    Number(u8),
+}
+
+impl Display for FizzBuzz {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match self {
+            FizzBuzz::FizzBuzz => write!(f, "FizzBuzz"),
+            FizzBuzz::Fizz => write!(f, "Fizz"),
+            FizzBuzz::Buzz => write!(f, "Buzz"),
+            FizzBuzz::Number(n) => write!(f, "{}", n.to_string()),
+        }
+    }
+}
+
+fn fizzbuzz(number: u8) -> FizzBuzz {
     match (number % 3, number % 5) {
-        (0, 0) => "fizzbuzz".to_string(),
-        (0, _) => "fizz".to_string(),
-        (_, 0) => "buzz".to_string(),
-        _ => number.to_string(),
+        (0, 0) => FizzBuzz::FizzBuzz,
+        (0, _) => FizzBuzz::Fizz,
+        (_, 0) => FizzBuzz::Buzz,
+        _ => FizzBuzz::Number(number),
     }
 }
 
 fn main() {
-    (1..=100)
-        .map(fizzbuzz)
-        .for_each(|result| println!("{}", result));
+    (1..=100).for_each(|number| println!("{}", fizzbuzz(number)));
 }
 
 #[cfg(test)]
@@ -19,26 +38,26 @@ mod fizzbuzz_tests {
 
     #[test]
     fn test_multiple_of_three_is_fizz() {
-        assert_eq!(fizzbuzz(3), "fizz");
-        assert_eq!(fizzbuzz(9), "fizz");
+        assert_eq!(fizzbuzz(3), FizzBuzz::Fizz);
+        assert_eq!(fizzbuzz(9), FizzBuzz::Fizz);
     }
 
     #[test]
     fn test_multiple_of_five_is_buzz() {
-        assert_eq!(fizzbuzz(5), "buzz");
-        assert_eq!(fizzbuzz(10), "buzz");
+        assert_eq!(fizzbuzz(5), FizzBuzz::Buzz);
+        assert_eq!(fizzbuzz(10), FizzBuzz::Buzz);
     }
 
     #[test]
     fn test_multiple_of_three_and_five_is_fizzbuzz() {
-        assert_eq!(fizzbuzz(15), "fizzbuzz");
-        assert_eq!(fizzbuzz(45), "fizzbuzz");
+        assert_eq!(fizzbuzz(15), FizzBuzz::FizzBuzz);
+        assert_eq!(fizzbuzz(45), FizzBuzz::FizzBuzz);
     }
 
     #[test]
     fn test_number_not_divisible_by_three_or_five_is_number() {
-        assert_eq!(fizzbuzz(1), "1");
-        assert_eq!(fizzbuzz(4), "4");
-        assert_eq!(fizzbuzz(98), "98");
+        assert_eq!(fizzbuzz(1), FizzBuzz::Number(1));
+        assert_eq!(fizzbuzz(4), FizzBuzz::Number(4));
+        assert_eq!(fizzbuzz(98), FizzBuzz::Number(98));
     }
 }
